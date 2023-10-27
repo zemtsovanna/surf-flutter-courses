1c88b100349fa609af08a15675cff4c97e01c970
annazemcova <zemtsovanna@gmail.com>
2023-10-27 14:14:47 +0300

    Предварительная работа перед фильтрами

---
+++ task-06/bin/task_06.dart
@@ -0,0 +1,97 @@
+import 'dart:math';
+
+main() {
+  final products = parseArticles(articles);
+
+  print(
+    applyFilter(products, RandomFilter()),
+  );
+
+  print(
+    applyFilter(products, IndexFilter(1)),
+  );
+}
+
+abstract interface class Filter {
+  bool apply(ProductModel product);
+}
+
+class RandomFilter extends Filter {
+  @override
+  bool apply(ProductModel product) {
+    return Random().nextBool();
+  }
+}
+
+class IndexFilter extends Filter {
+  final int index;
+
+  IndexFilter(this.index);
+  @override
+  bool apply(ProductModel product) {
+    return product.id == index;
+  }
+}
+
+List<ProductModel> applyFilter(List<ProductModel> products, Filter filter) {
+  return [...products]..removeWhere((element) => filter.apply(element));
+}
+// ----------------------------------------------------------------------------
+
+List<ProductModel> parseArticles(String articles) {
+  final tempList = articles.split('\n');
+  tempList.removeWhere((element) => element == '');
+
+  final result = tempList.map((rawProduct) {
+    final fields = rawProduct.split(',');
+    return ProductModel(
+      id: int.parse(fields[0]),
+      category: fields[1],
+      name: fields[2],
+      price: int.parse(fields[3]),
+      quantity: int.parse(fields[4]),
+    );
+  }).toList();
+
+  return result;
+}
+
+/// Модель продукта.
+class ProductModel {
+  ///  Id.
+  final int id;
+
+  ///  Категория.
+  final String category;
+
+  ///  Наименование.
+  final String name;
+
+  ///  Цена.
+  final int price;
+
+  ///  Количество.
+  final int quantity;
+
+  @override
+  String toString() {
+    return '$id $category  $name  $price $quantity';
+  }
+
+  ProductModel({
+    required this.id,
+    required this.category,
+    required this.name,
+    required this.price,
+    required this.quantity,
+  });
+}
+
+final articles = '''
+1,хлеб,Бородинский,500,5
+2,хлеб,Белый,200,15
+3,молоко,Полосатый кот,50,53
+4,молоко,Коровка,50,53
+5,вода,Апельсин,25,100
+6,вода,Бородинский,500,5
+''';
